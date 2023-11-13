e401e7e268f42ac538d41390086e6cf39d22de5f
Alan16263 <jorge.gomez193@tectijuana.edu.mx>
2023-11-13 11:04:07 +0000

    Boton de editar

--- collection/forms.py
+++ collection/forms.py
@@ -5,13 +5,6 @@ class CollectionForm(forms.Form):
     name = forms.CharField(label="Nombre", max_length=80, required=True)
     description = forms.CharField(label="Descripción", required=True)
 
-    
-class AgregarAColeccionForm(forms.Form):
-    collection = forms.ModelChoiceField(
-        queryset=Collection.objects.none(),
-        required=False,
-        empty_label="Todas las colecciones"
-    )
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
